1d4df211f84b0dbb2326a8aa39429f11b4f48f4a
Andrew <arhautau@gmail.com>
2013-10-18 23:46:51 -0700

    better comments in packages.r

--- lib/packages.r
+++ lib/packages.r
@@ -5,21 +5,29 @@ pkgTest <- function(x) {
     if(!require(x,character.only = TRUE)) stop("Package not found") 
       }
 }
+
 #pkgTest('outliers')
+
 # for svm
 #pkgTest('e1071')
+
 # spare matrix helpers
 #pkgTest('SparseM')
-# ggplot, for great looking plots!
 
+# ggplot, for great looking plots!
 #pkgTest('ggplot2')
+
 # ROC convenience
 #pkgTest('ROCR')
+
 # clustering 
 #pkgTest('cluster') 
+
 # various methods for clustering
 #pkgTest('fpc')
+
 # random forest convenience
 #pkgTest('randomForest')
+
 # lots of helpers for analysis and machine learning, even ensembling!
 #pkgTest('caret')
